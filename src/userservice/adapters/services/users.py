a1e240604def8e770d4471295b0d61ed7eea0ea2
Shako Rzayev <shahriyar.rzayev@nordsec.com>
2023-04-25 21:58:29 +0200

    feat: add interface for user service

--- src/userservice/adapters/services/users.py
+++ src/userservice/adapters/services/users.py
@@ -4,10 +4,11 @@ from eventsourcing.application import AggregateNotFound, Application
 
 from userservice.domain.model.aggregate import User
 from userservice.domain.model.exceptions import UserAccountNotFoundError
+from userservice.domain.ports.services.users import UsersServiceInterface
 
 
-class UsersService(Application):
-    def register_user(self, full_name: str, email: str) -> UUID:
+class UsersService(Application, UsersServiceInterface):
+    def _register_user(self, full_name: str, email: str) -> UUID:
         user = User.register(
             full_name=full_name,
             email=email,
@@ -15,7 +16,7 @@ class UsersService(Application):
         self.save(user)
         return user.id
 
-    def get_user(self, user_id: UUID) -> User:
+    def _get_user(self, user_id: UUID) -> User:
         try:
             aggregate = self.repository.get(user_id)
         except AggregateNotFound as err:
@@ -24,13 +25,13 @@ class UsersService(Application):
             assert isinstance(aggregate, User)
             return aggregate
 
-    def update_user(self, user_id: UUID, full_name: str) -> User:
+    def _update_user(self, user_id: UUID, full_name: str) -> User:
         aggregate = self.get_user(user_id)
         aggregate.update(full_name)
         self.save(aggregate)
         return aggregate
 
-    def deactivate_user(self, user_id: UUID) -> User:
+    def _deactivate_user(self, user_id: UUID) -> User:
         aggregate = self.get_user(user_id)
         aggregate.deactivate()
         self.save(aggregate)
---
+++ src/userservice/domain/ports/services/users.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+
+from userservice.domain.model.aggregate import User
+
+
+class UsersServiceInterface(ABC):
+    def register_user(self, full_name: str, email: str) -> UUID:
+        return self._register_user(full_name, email)
+
+    def get_user(self, user_id: UUID) -> User:
+        return self._get_user(user_id)
+
+    def update_user(self, user_id: UUID, full_name: str) -> User:
+        return self._update_user(user_id, full_name)
+
+    def deactivate_user(self, user_id: UUID) -> User:
+        return self._deactivate_user(user_id)
+
+    @abstractmethod
+    def _register_user(self, full_name: str, email: str) -> UUID:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _get_user(self, user_id: UUID) -> User:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _update_user(self, user_id: UUID, full_name: str) -> User:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _deactivate_user(self, user_id: UUID) -> User:
+        raise NotImplementedError
